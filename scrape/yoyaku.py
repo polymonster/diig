4af2d66e8198b3346457cf646eff63f04e6483f5
polymonster <alexandercdixon@gmail.com>
2024-02-18 20:57:28 +0000

    - add tracker info to yoyaku

--- scrape/yoyaku.py
+++ scrape/yoyaku.py
@@ -38,70 +38,75 @@ def scrape_page(url, store, view, section, counter = 0):
         release_dict["link"] = dig.get_value(release, "href")
         release_dict["title"] = dig.get_value(release, "aria-label")
         release_dict["id"] = os.path.basename(release_dict["link"].strip("/"))
-        srcset = dig.get_value(release, "srcset")
+        key = f'{release_dict["store"]}-{release_dict["id"]}'
 
-        # images order
-        target_w = [
-            "100w",
-            "400w",
-            "768w"
-        ]
+        if release_dict["id"] not in releases_dict:
+            srcset = dig.get_value(release, "srcset")
 
-        release_dict["artworks"] = list()
-        imgs = srcset.split(",")
-        for w in target_w:
-            for img in imgs:
-                if img.endswith(w):
-                    release_dict["artworks"].append(img.strip(w).strip())
+            # images order
+            target_w = [
+                "100w",
+                "400w",
+                "768w"
+            ]
 
-        # detail info
-        try:
-            req = urllib.request.Request(
-                url=release_dict["link"],
-                headers={'User-Agent': 'Mozilla/5.0'}
-            )
-            release_html_file = urllib.request.urlopen(req)
-        except:
-            print("error: url not found {}".format(url))
-            return
+            release_dict["artworks"] = list()
+            imgs = srcset.split(",")
+            for w in target_w:
+                for img in imgs:
+                    if img.endswith(w):
+                        release_dict["artworks"].append(img.strip(w).strip())
 
-        # parse release page
-        release_html_str = release_html_file.read().decode("utf8")
+            # detail info
+            try:
+                req = urllib.request.Request(
+                    url=release_dict["link"],
+                    headers={'User-Agent': 'Mozilla/5.0'}
+                )
+                release_html_file = urllib.request.urlopen(req)
+            except:
+                print("error: url not found {}".format(url))
+                return
 
-        # artist info
-        pp = release_html_str.find("class=\"product-artists\"")
-        pe = release_html_str.find("</span>", pp)
-        release_dict["artist"] = dig.parse_strip_body(release_html_str[pp:pe])
+            # parse release page
+            release_html_str = release_html_file.read().decode("utf8")
 
-        # label info
-        pp = release_html_str.find("class=\"product-labels\"")
-        pe = release_html_str.find("</span>", pp)
-        release_dict["label"] = dig.parse_strip_body(release_html_str[pp:pe])
-        release_dict["label_link"] = dig.get_value(release_html_str[pp:pe], "href")
+            # artist info
+            pp = release_html_str.find("class=\"product-artists\"")
+            pe = release_html_str.find("</span>", pp)
+            release_dict["artist"] = dig.parse_strip_body(release_html_str[pp:pe])
 
-        # cat
-        pp = release_html_str.find("class=\"sku\"")
-        pe = release_html_str.find("</span>", pp)
-        release_dict["cat"] = dig.parse_strip_body(release_html_str[pp:pe])
+            # label info
+            pp = release_html_str.find("class=\"product-labels\"")
+            pe = release_html_str.find("</span>", pp)
+            release_dict["label"] = dig.parse_strip_body(release_html_str[pp:pe])
+            release_dict["label_link"] = dig.get_value(release_html_str[pp:pe], "href")
 
-        # tracklist
-        release_dict["track_names"] = list()
-        tracklist = dig.parse_class(release_html_str, "class=\"track fwap-play\"", "a")
-        for track in tracklist:
-            release_dict["track_names"].append(dig.parse_strip_body(track))
+            # cat
+            pp = release_html_str.find("class=\"sku\"")
+            pe = release_html_str.find("</span>", pp)
+            release_dict["cat"] = dig.parse_strip_body(release_html_str[pp:pe])
 
-        # track urls
-        cdn = "https://yydistribution.ams3.digitaloceanspaces.com/yyplayer/mp3"
-        cdn_id = release_dict["id"].upper()
+            # tracklist
+            release_dict["track_names"] = list()
+            tracklist = dig.parse_class(release_html_str, "class=\"track fwap-play\"", "a")
+            for track in tracklist:
+                release_dict["track_names"].append(dig.parse_strip_body(track))
 
-        release_dict["track_urls"] = list()
-        for i in range(0, len(tracklist)):
-            release_dict["track_urls"].append(f"{cdn}/{cdn_id}_{i}.mp3")
+            # track urls
+            cdn = "https://yydistribution.ams3.digitaloceanspaces.com/yyplayer/mp3"
+            cdn_id = release_dict["id"].upper()
+
+            release_dict["track_urls"] = list()
+            for i in range(0, len(tracklist)):
+                release_dict["track_urls"].append(f"{cdn}/{cdn_id}_{i}.mp3")
+
+        # assign pos per section
+        release_dict[f"{store}-{section}-{view}"] = int(counter)
 
         counter += 1
 
         # merge into main
-        key = f'{release_dict["store"]}-{release_dict["id"]}'
         merge = dict()
         merge[key] = release_dict
         dig.merge_dicts(releases_dict, merge)

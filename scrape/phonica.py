069a84d4b41c0e2328e4e0a150b96464e1b48c2f
polymonster <alexandercdixon@gmail.com>
2025-10-06 18:36:08 +0100

    - the remaining data

--- scrape/phonica.py
+++ scrape/phonica.py
@@ -25,6 +25,7 @@ def cache_page(url, cache_file):
 def scrape_product(release):
     product_html = cache_page("https://www.phonicarecords.com/product/doc-n-p1ll-rx-prescriptions-volume-2-serenity-now/205828/", "phonica_product.html")
 
+    # find track names and urls
     offset = 0
     (offset, playlist_elems)= dig.find_parse_nested_elems(product_html, offset, '<span class="archive-playlist display-none">', "<span", "</span>")
 
@@ -44,6 +45,16 @@ def scrape_product(release):
     release["track_names"] = track_names
     release["track_urls"] = track_urls
 
+    # find artwork
+    offset = 0
+    (offset, artwork_elems)= dig.find_parse_nested_elems(product_html, offset, '<meta property="og:image"', "<meta", "</meta>")
+
+    release["artworks"] = [
+        dig.get_value(artwork_elems, "content"),
+        dig.get_value(artwork_elems, "content"),
+        dig.get_value(artwork_elems, "content"),
+    ]
+
 
 # scrape an individual page for a view (essentials, new releases)
 def scrape_page(url, store, view, section, counter, session_scraped_ids):
@@ -54,11 +65,10 @@ def scrape_page(url, store, view, section, counter, session_scraped_ids):
     releases = dict()
 
     # TODO:
-    # id
-    # label_link
-    # cat
-    # artworks
-    # store_tags
+    # store_tags : {
+    #   out_of_stock
+    #   preorder
+    #}
 
     product_group = dig.parse_div(porducts_html, 'id="archive-grid-view-home-category-6"')
     for group in product_group:
@@ -69,16 +79,25 @@ def scrape_page(url, store, view, section, counter, session_scraped_ids):
             (offset, artist_elem) = dig.find_parse_elem(p, offset, '<a id=', "</a>")
             (offset, product_link_elem) = dig.find_parse_elem(p, offset, '<a id=', "</a>")
             (offset, label_elem) = dig.find_parse_elem(p, offset, '<a id=', "</a>")
+            (offset, stock_elem) = dig.find_parse_elem(p, offset, '<i id="stock-indicator', "</a>")
 
             offset = 0
             (offset, title_elem) = dig.find_parse_elem(product_link_elem, offset, '<span class', "</span>")
 
             release = dict()
             release["store"] = "phonica"
+            release["id"] = "phonica-" + dig.get_value(image_elem, "id")
             release["title"] = dig.parse_body(title_elem).strip()
             release["link"] = dig.get_value(product_link_elem, "href").strip()
             release["artist"] = dig.parse_body(artist_elem).strip()
             release["label"] = dig.parse_body(label_elem).strip()
+            release["label_link"] = dig.get_value(label_elem, "href").strip()
+            release["cat"] = ""
+
+            release["store_tags"] = {
+                "out_of_stock": stock_elem.find("circle-shape-red") != -1,
+                "preorder": stock_elem.find("circle-shape-blue") != -1
+            }
 
             scrape_product(release)
 

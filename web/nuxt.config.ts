92a9d51f278cf94212dd59cb6a9974134b566096
ooOsH <github@nickwaller.co.uk>
2025-12-18 16:00:20 +0000

    🪓 Chore - Add service account and app config

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -18,13 +18,18 @@ export default defineNuxtConfig({
   },
 
   vuefire: {
+    auth: {
+      enabled: true
+    },
     config: {
-      // apiKey: process.env.FIREBASE_API_KEY,
-      // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID ?? 'diig-web',
-      // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      // appId: process.env.FIREBASE_APP_ID,
+      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NUXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NUXT_PUBLIC_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId:process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     },
   },
 })

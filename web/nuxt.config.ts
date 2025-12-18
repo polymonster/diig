b401a37291efe2ba81449d2540620b467688393e
Nicholas Waller <github@nickwaller.co.uk>
2025-12-18 15:16:10 +0000

    🐛 Fix Bug - Missing module and add to config (#5)

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -17,14 +17,14 @@ export default defineNuxtConfig({
     viewer: true,
   },
 
-  // vuefire: {
-  //   config: {
-  //     apiKey: process.env.FIREBASE_API_KEY,
-  //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //     projectId: process.env.FIREBASE_PROJECT_ID,
-  //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  //     appId: process.env.FIREBASE_APP_ID,
-  //   },
-  // },
+  vuefire: {
+    config: {
+      // apiKey: process.env.FIREBASE_API_KEY,
+      // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID ?? 'diig-web',
+      // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      // appId: process.env.FIREBASE_APP_ID,
+    },
+  },
 })

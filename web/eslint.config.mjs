f19664890e8e89008159de0dca76465033e72b73
polymonster <alexandercdixon@gmail.com>
2026-04-26 20:29:29 +0100

    - fix web ci

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,11 +1,4 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-    }
-  }
-)
+export default withNuxt()
